60bb1c8dabeb188bbb6cf9ed2fd838454be13f8f
fiRinhozor <bulatgilfanov0@gmail.com>
2022-03-13 21:40:05 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,48 @@
+const squares = document.querySelectorAll(".square");
+const mole = document.querySelector(".mole");
+const timeLeft = document.querySelector("#time-left");
+const yourScore = document.querySelector("#score");
+
+let result = 0;
+let hitPosition;
+let currentTime = 60;
+let timerId = null;
+
+function randomSquare() {
+  squares.forEach((square) => {
+    square.classList.remove("mole");
+  });
+  let randomPosition = squares[Math.floor(Math.random() * 9)];
+  randomPosition.classList.add("mole");
+
+  hitPosition = randomPosition.id;
+}
+
+squares.forEach((square) => {
+  square.addEventListener("mousedown", () => {
+    if (square.id == hitPosition) {
+      result++;
+      yourScore.textContent = result;
+      hitPosition = null;
+    }
+  });
+});
+
+function moveMole() {
+  timerId = setInterval(randomSquare, 700);
+}
+
+moveMole();
+
+function countDown() {
+  currentTime--;
+  timeLeft.textContent = currentTime;
+
+  if (currentTime == 0) {
+    clearInterval(countDownTimerId);
+    clearInterval(timerId);
+    alert("Game Over! Your final score is " + result);
+  }
+}
+
+let countDownTimerId = setInterval(countDown, 1000);
